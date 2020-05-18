7a3a5c5a63dedbdd0abf0d152f30aaf984d8b9e6
claudomiro <claudomiro@gmail.com>
2020-05-18 10:56:01 -0300

    Passo 11 - Transformando os dados do DTO em entidades - municípios
    Especificação

---
+++ src/test/java/org/codigolimpo/IBGE/domain/MunicipalityTests.java
@@ -0,0 +1,24 @@
+package org.codigolimpo.IBGE.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.codigolimpo.IBGE.TestConstants.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class MunicipalityTests {
+
+    @Test
+    public void whenValidDTOsCreateMunicipality() {
+        Municipality abreCampo = MINAS.createMunicipalityFromDTO(DTO_ABRECAMPO);
+        Municipality anotherAbreCampo = MINAS.createMunicipalityFromDTO(DTO_ABRECAMPO);
+        Municipality abadia = MINAS.createMunicipalityFromDTO(DTO_ABADIA);
+        assertThat(abreCampo.getId(), is(nullValue()));
+        assertThat(abreCampo.getFederalUnit(), equalTo(MINAS));
+        assertThat(abreCampo.getIdIBGE(), equalTo(ID_IBGE_ABRECAMPO));
+        assertThat(abreCampo.getName(), equalTo(NAME_ABRECAMPO));
+        assertThat(abreCampo, equalTo(anotherAbreCampo));
+        assertThat(abreCampo, is(not(equalTo(abadia))));
+        assertThat(MINAS.getMunicipios(), contains(abreCampo, anotherAbreCampo, abadia));
+    }
+}
