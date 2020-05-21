c1e38ef94b30e68e719581a17b482e0d37dd59b0
claudomiro <claudomiro@gmail.com>
2020-05-21 09:32:25 -0300

    Passo 11 - Transformando os dados do DTO em entidades - municípios
    Implementação

--- src/main/java/org/codigolimpo/IBGE/domain/Municipality.java
+++ src/main/java/org/codigolimpo/IBGE/domain/Municipality.java
@@ -1,37 +1,37 @@
 package org.codigolimpo.IBGE.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 import org.codigolimpo.IBGE.domain.DTO.MunicipioDTO;
+import org.hibernate.annotations.NaturalId;
 
-import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 @Getter
+@ToString
+@EqualsAndHashCode
 public class Municipality {
-
+    @Id
+    @GeneratedValue
+    @EqualsAndHashCode.Exclude
     private Long id;
+
+    @ManyToOne
     private FederalUnit federalUnit;
+
+    @NaturalId
     private int idIBGE;
     String name;
 
-    public Municipality(FederalUnit federalUnit, MunicipioDTO dto) {
+    Municipality(FederalUnit federalUnit, MunicipioDTO dto) {
         this.federalUnit = federalUnit;
         this.idIBGE = dto.getId();
         this.name = dto.getNome();
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Municipality that = (Municipality) o;
-        return getIdIBGE() == that.getIdIBGE() &&
-                Objects.equals(getId(), that.getId()) &&
-                Objects.equals(getFederalUnit().getIdIBGE(), that.getFederalUnit().getIdIBGE()) &&
-                Objects.equals(getName(), that.getName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getFederalUnit().getIdIBGE(), getIdIBGE(), getName());
-    }
 }
---
+++ src/main/java/org/codigolimpo/IBGE/repository/MunicipalityRepository.java
@@ -0,0 +1,7 @@
+package org.codigolimpo.IBGE.repository;
+
+import org.codigolimpo.IBGE.domain.Municipality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MunicipalityRepository extends CrudRepository<Municipality, Integer> {
+}
