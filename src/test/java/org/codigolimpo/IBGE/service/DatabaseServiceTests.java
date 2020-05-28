9e1f1213ed6e25af99ddffb8fa69952d4864462f
claudomiro <claudomiro@gmail.com>
2020-05-28 16:42:24 -0300

    Passo 14 - Criando testes unitários (usando mocks) no banco
    Refactoring

--- src/test/java/org/codigolimpo/IBGE/service/DatabaseServiceTests.java
+++ src/test/java/org/codigolimpo/IBGE/service/DatabaseServiceTests.java
@@ -4,17 +4,16 @@ import org.codigolimpo.IBGE.domain.FederalUnit;
 import org.codigolimpo.IBGE.domain.Municipality;
 import org.codigolimpo.IBGE.repository.FederalUnitRepository;
 import org.codigolimpo.IBGE.repository.MunicipalityRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.codigolimpo.IBGE.TestConstants.DTO_FEDERAL_UNIT_TEST;
-import static org.codigolimpo.IBGE.TestConstants.DTO_MUNICIPIO_TEST;
+import static org.codigolimpo.IBGE.TestConstants.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.Mockito.*;
 
@@ -30,27 +29,31 @@ public class DatabaseServiceTests {
     @InjectMocks
     private DatabaseServiceImpl service;
 
+    private FederalUnit testFederalUnit;
+
+    @BeforeEach
+    void setUp() {
+        testFederalUnit = FederalUnit.createFromDTO(DTO_FEDERAL_UNIT_TEST);
+        when(federalUnitRepository.save(any(FederalUnit.class))).then(returnsFirstArg());
+    }
+
     @Test
     public void whenValidFederalUnitEntitySave() {
-        when(federalUnitRepository.save(any(FederalUnit.class))).then(returnsFirstArg());
-        FederalUnit testFederalUnit = FederalUnit.createFromDTO(DTO_FEDERAL_UNIT_TEST);
-
-        FederalUnit saved = service.saveFederalUnit(testFederalUnit);
+        service.saveFederalUnit(testFederalUnit);
 
         verify(federalUnitRepository).save(testFederalUnit);
     }
 
     @Test
     public void whenValidMunicipalityDTOSaveAndAddObject() {
-        when(federalUnitRepository.save(any(FederalUnit.class))).then(returnsFirstArg());
         when(municipalityRepository.save(any(Municipality.class))).then(returnsFirstArg());
-        FederalUnit testFU = FederalUnit.createFromDTO(DTO_FEDERAL_UNIT_TEST);
-        service.saveFederalUnit(testFU);
-        Municipality dto = testFU.createMunicipalityFromDTO(DTO_MUNICIPIO_TEST);
-        service.saveMunicipality(dto);
-        verify(municipalityRepository).save(dto);
 
-        assertThat(testFU.getMunicipalities(), contains(dto));
+        Municipality testMunicipality = testFederalUnit.createMunicipalityFromDTO(DTO_MUNICIPIO_TEST);
+        service.saveFederalUnit(testFederalUnit);
+        service.saveMunicipality(testMunicipality);
+
+        verify(municipalityRepository).save(testMunicipality);
+        assertThat(testFederalUnit.getMunicipalities(), contains(testMunicipality));
     }
 
 }
